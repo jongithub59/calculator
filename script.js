a58a4c171022bb5caee5b13c3de0f25c601457ad
jongithub59 <jdelrealj01@gmail.com>
2023-10-20 10:52:14 -0500

    added pseudocode for javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+//when a button is pressed
+//add its value to a variable
+//display it to the display window
+//if decimal is pressed add a "." to the current number but only once per number
+//when equal is pressed calculate the result and display it
+//when an operation button is pressed display the result of the previous pair of numbers and use the result for the next input
+//if all clear is pressed clear tthe display
+//if clear is pressed delete the previously added input only
