6931070a05f0dc64d29d41f36d475845c745fbef
jongithub59 <jdelrealj01@gmail.com>
2023-10-23 12:02:31 -0500

    updated newNumber() and addOperation() added spans changed newNumber() and addOperation() to add the text content of the buttons to the display and save them to variables using span elememts to keep the numbers and operator separate.

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let firstNumber = 5
-let secondNumber = 3
-let currentOperator = ''
+let firstNumber = '';
+let secondNumber = '';
+let currentOperator = '';
 const clearDisplay = document.getElementById('clear-all');
 const clear = document.getElementById('clear');
 const equal = document.getElementById('equal');
 const numButtons = document.querySelectorAll('[number]');
 const opButtons = document.querySelectorAll('[operator]');
-const calcDisplay = document.getElementById('calculation');
+const sectionOne = document.getElementById('first-number')
+const sectionTwo = document.getElementById('operator')
+const sectionThree = document.getElementById('second-number');
 const resultDisplay = document.getElementById('result');
-console.log(numButtons);
-console.log(opButtons);
 //when a button is pressed
 clearDisplay.addEventListener('click', clearAll);
 clear.addEventListener('click', clearEntry);
 equal.addEventListener('click', calculate);
+
 
 numButtons.forEach((button) =>
     button.addEventListener('click', () => newNumber(button.textContent))
@@ -29,17 +30,25 @@ function clearEntry() {
 
 //add its value to a variable
 function newNumber(number) {
-    let firstNumber = number
-    calcDisplay.innerHTML += firstNumber + ' ';
+    if (currentOperator != '') {
+    sectionThree.innerHTML += number;
+    secondNumber = sectionThree.innerHTML;
+    console.log(secondNumber);
+    } else {
+    sectionOne.innerHTML += number;
+    firstNumber = sectionOne.innerHTML;
+    }
 }
 
 function addOperation(operator) {
-    calcDisplay.innerHTML += operator + ' ';
+    if (currentOperator != '') return;
+    sectionTwo.innerHTML += ' ' + operator + ' ';
+    currentOperator = operator;
 }
 //if decimal is pressed add a "." to the current number but only once per number
 //when equal is pressed calculate the result and display it
-function calculate(operator, firstNumber, secondNumber) {
-    switch(operator) {
+function calculate(currentOperator, firstNumber, secondNumber) {
+    switch (currentOperator) {
         case '+':
             add(firstNumber, secondNumber);
         break;
@@ -47,10 +56,10 @@ function calculate(operator, firstNumber, secondNumber) {
             subtract(firstNumber, secondNumber);
         break;
         case '×':
-            multiply(firstNumber, secondNumber);
+            resultDisplay.innerHTML = multiply(firstNumber, secondNumber);
         break;
         case '÷':
-            divie(firstNumber, secondNumber);
+            divide(firstNumber, secondNumber);
         break;
         case '^':
             exponent(firstNumber, secondNumber);
@@ -62,29 +71,29 @@ function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
 function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber
+    return firstNumber - secondNumber;
 }
 function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber
+    return firstNumber * secondNumber;
 }
 function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber
+    return firstNumber / secondNumber;
 }
 function exponent(firstNumber, secondNumber) {
-    return firstNumber ** secondNumber
+    return firstNumber ** secondNumber;
 }
 //when an operation button is pressed display the result of the previous pair of numbers and use the result for the next input
 //if all clear is pressed clear the display
 function clearAll() {
-    calcDisplay.innerHTML = '';
+    sectionOne.innerHTML = ''
+    sectionTwo.innerHTML = ''
+    sectionThree.innerHTML = ''
     resultDisplay.innerHTML = '';
+    firstNumber = '';
+    secondNumber = '';
+    currentOperator = '';
 }
 //if clear is pressed delete the previously added input only
 function clearEntry() {
 
 }
-console.log(add(firstNumber, secondNumber))
-console.log(subtract(firstNumber, secondNumber))
-console.log(multiply(firstNumber, secondNumber))
-console.log(divide(firstNumber, secondNumber))
-console.log(exponent(firstNumber, secondNumber))
